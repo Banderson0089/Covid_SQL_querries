6979922283de9c385a88f60bbc0f2997ebe3a31b
Banderson0089 <87945015+Banderson0089@users.noreply.github.com>
2024-04-22 15:25:31 -0600

    Add files via upload

---
+++ Volumetrics (cdos) (4) (1).sql
@@ -0,0 +1,131 @@
+-- Databricks notebook source
+-- MAGIC %scala
+-- MAGIC dbutils.widgets.dropdown("targetMarket", "NY", Seq("OH", "NY", "IN","OR","CT","WA", "SC", "GA","WI", "VA", "TN"), "Target Market");
+-- MAGIC
+-- MAGIC var targetMarket = dbutils.widgets.get("targetMarket")
+-- MAGIC
+-- MAGIC dbutils.widgets.dropdown("targetDomain", "medclaim_header_hist", Seq("medclaim_header_hist", "lab_result_hist", "pharmacy_hist", "eligibility_hist", "patient_hist", "medclaim_details_hist", "provider_hist"), "Target Domain");
+-- MAGIC
+-- MAGIC var targetDomain = dbutils.widgets.get("targetDomain")
+-- MAGIC
+-- MAGIC dbutils.widgets.dropdown("File_export", "CDOS", Seq("CDOS", "CDOS_ARCADIA", "CDOS_WELLMED", "cdos_dry_run"), "File_export");
+-- MAGIC
+-- MAGIC var File_export = dbutils.widgets.get("File_export")
+-- MAGIC
+-- MAGIC
+
+-- COMMAND ----------
+
+-- DBTITLE 1,total counts
+-- MAGIC
+-- MAGIC %scala
+-- MAGIC
+-- MAGIC var df = spark.sql("""select distinct loadtime from """+File_export+"""_history_validation."""+targetMarket+"""_"""+targetDomain+""" WHERE loadtime >= date_sub(current_date(), 120) ORDER BY loadtime desc""")
+-- MAGIC val loadtime_list = (df.select("loadtime").collect().map(_.getString(0)).mkString("','")).mkString
+-- MAGIC println(loadtime_list)
+-- MAGIC
+-- MAGIC var query = spark.sql("""
+-- MAGIC
+-- MAGIC SELECT *FROM (
+-- MAGIC SELECT loadtime, source_system_sk, count(source_system_sk) as count_source_system_sk
+-- MAGIC
+-- MAGIC FROM """+File_export+"""_history_validation."""+targetMarket+"""_"""+targetDomain+"""
+-- MAGIC WHERE loadtime >= date_sub(current_date(), 120)
+-- MAGIC --and (GMPI is null or GMPI ='')
+-- MAGIC and (record_start_dt < current_date 
+-- MAGIC and nvl(record_end_dt,'9999-12-31')>current_date)
+-- MAGIC  GROUP BY loadtime, source_system_sk
+-- MAGIC )
+-- MAGIC
+-- MAGIC PIVOT ( SUM(count_source_system_sk)
+-- MAGIC FOR loadtime IN ('"""+loadtime_list+"""')
+-- MAGIC )
+-- MAGIC
+-- MAGIC """)
+-- MAGIC display(query)
+
+-- COMMAND ----------
+
+-- DBTITLE 1,distinct entity member
+-- MAGIC %scala
+-- MAGIC
+-- MAGIC var df = spark.sql("""select distinct loadtime from """+File_export+"""_history_validation."""+targetMarket+"""_"""+targetDomain+""" WHERE loadtime >= date_sub(current_date(), 70) ORDER BY loadtime desc""")
+-- MAGIC val loadtime_list = (df.select("loadtime").collect().map(_.getString(0)).mkString("','")).mkString
+-- MAGIC println(loadtime_list)
+-- MAGIC
+-- MAGIC var query = spark.sql("""
+-- MAGIC
+-- MAGIC SELECT *FROM (
+-- MAGIC SELECT loadtime, source_system_sk, count(distinct entity_member_id) as count_entity_member_ID
+-- MAGIC
+-- MAGIC FROM """+File_export+"""_history_validation."""+targetMarket+"""_"""+targetDomain+"""
+-- MAGIC WHERE loadtime >= date_sub(current_date(), 70)
+-- MAGIC --and record_start_dt < current_date and nvl(record_end_dt,'9999-12-31')>current_date
+-- MAGIC  GROUP BY loadtime, source_system_sk
+-- MAGIC )
+-- MAGIC
+-- MAGIC PIVOT ( SUM(count_entity_member_ID)
+-- MAGIC FOR loadtime IN ('"""+loadtime_list+"""')
+-- MAGIC )
+-- MAGIC
+-- MAGIC """)
+-- MAGIC display(query)
+
+-- COMMAND ----------
+
+-- MAGIC %scala
+-- MAGIC
+-- MAGIC var df = spark.sql("""select distinct loadtime from """+File_export+"""_history_validation."""+targetMarket+"""_"""+targetDomain+""" WHERE loadtime >= date_sub(current_date(), 80) ORDER BY loadtime desc""")
+-- MAGIC val loadtime_list = (df.select("loadtime").collect().map(_.getString(0)).mkString("','")).mkString
+-- MAGIC //println(loadtime_list) 
+-- MAGIC
+-- MAGIC var query = spark.sql("""
+-- MAGIC
+-- MAGIC SELECT *FROM (
+-- MAGIC SELECT loadtime, health_plan, count(health_plan) as count_health_plan
+-- MAGIC FROM """+File_export+"""_history_validation."""+targetMarket+"""_"""+targetDomain+"""
+-- MAGIC WHERE loadtime >= date_sub(current_date(), 80) GROUP BY loadtime, health_plan
+-- MAGIC )
+-- MAGIC
+-- MAGIC PIVOT ( SUM(count_health_plan)
+-- MAGIC FOR loadtime IN ('"""+loadtime_list+"""')
+-- MAGIC )
+-- MAGIC
+-- MAGIC """)
+-- MAGIC display(query)
+-- MAGIC
+
+-- COMMAND ----------
+
+-- MAGIC %scala
+-- MAGIC
+-- MAGIC var df =  spark.sql (""" 
+-- MAGIC select distinct Filename from """+File_export+"""_history_validation."""+targetMarket+"""_provider_hist WHERE LoadTime in (select MAX(LoadTime) AS LoadTime from """+File_export+"""_history_validation."""+targetMarket+"""_provider_hist)
+-- MAGIC union
+-- MAGIC select distinct Filename from """+File_export+"""_history_validation."""+targetMarket+"""_eligibility_hist WHERE LoadTime in (select MAX(LoadTime) AS LoadTime from """+File_export+"""_history_validation."""+targetMarket+"""_eligibility_hist)
+-- MAGIC union
+-- MAGIC select distinct Filename from """+File_export+"""_history_validation."""+targetMarket+"""_empi_hist WHERE LoadTime in (select MAX(LoadTime) AS LoadTime from """+File_export+"""_history_validation."""+targetMarket+"""_empi_hist)
+-- MAGIC union
+-- MAGIC select distinct Filename from """+File_export+"""_history_validation."""+targetMarket+"""_incr_mbrhicnhistory_hist WHERE LoadTime in (select MAX(LoadTime) AS LoadTime from """+File_export+"""_history_validation."""+targetMarket+"""_incr_mbrhicnhistory_hist)
+-- MAGIC union
+-- MAGIC select distinct Filename from """+File_export+"""_history_validation."""+targetMarket+"""_inscope_patient_hist WHERE LoadTime in (select MAX(LoadTime) AS LoadTime from """+File_export+"""_history_validation."""+targetMarket+"""_inscope_patient_hist)
+-- MAGIC union
+-- MAGIC select distinct Filename from """+File_export+"""_history_validation."""+targetMarket+"""_lab_result_hist WHERE LoadTime in (select MAX(LoadTime) AS LoadTime from """+File_export+"""_history_validation."""+targetMarket+"""_lab_result_hist)
+-- MAGIC union
+-- MAGIC select distinct Filename from """+File_export+"""_history_validation."""+targetMarket+"""_location_hist WHERE LoadTime in (select MAX(LoadTime) AS LoadTime from """+File_export+"""_history_validation."""+targetMarket+"""_location_hist)
+-- MAGIC union
+-- MAGIC select distinct Filename from """+File_export+"""_history_validation."""+targetMarket+"""_medclaim_details_hist WHERE LoadTime in (select MAX(LoadTime) AS LoadTime from """+File_export+"""_history_validation."""+targetMarket+"""_medclaim_details_hist)
+-- MAGIC union
+-- MAGIC select distinct Filename from """+File_export+"""_history_validation."""+targetMarket+"""_medclaim_header_hist WHERE LoadTime in (select MAX(LoadTime) AS LoadTime from """+File_export+"""_history_validation."""+targetMarket+"""_medclaim_header_hist)
+-- MAGIC union
+-- MAGIC select distinct Filename from """+File_export+"""_history_validation."""+targetMarket+"""_patient_hist WHERE LoadTime in (select MAX(LoadTime) AS LoadTime from """+File_export+"""_history_validation."""+targetMarket+"""_patient_hist)
+-- MAGIC union
+-- MAGIC select distinct Filename from """+File_export+"""_history_validation."""+targetMarket+"""_pharmacy_hist WHERE LoadTime in (select MAX(LoadTime) AS LoadTime from """+File_export+"""_history_validation."""+targetMarket+"""_pharmacy_hist)
+-- MAGIC union
+-- MAGIC select distinct Filename from """+File_export+"""_history_validation."""+targetMarket+"""_pod_hist WHERE LoadTime in (select MAX(LoadTime) AS LoadTime from """+File_export+"""_history_validation."""+targetMarket+"""_pod_hist)
+-- MAGIC union
+-- MAGIC select distinct Filename from """+File_export+"""_history_validation."""+targetMarket+"""_provider_group_hist WHERE LoadTime in (select MAX(LoadTime) AS LoadTime from """+File_export+"""_history_validation."""+targetMarket+"""_provider_group_hist)
+-- MAGIC
+-- MAGIC """)
+-- MAGIC display(df)
+-- MAGIC
